88ea29b7f11acb63616c3d4ee2d57b7c6e4fc5e0
João Pedro <jpedrosousans@gmail.com>
2026-09-09 21:48:26 -0300

    feat: adiciona Aula01OlaDevDojo com metodo main, println e comentarios

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+        /**
+         * isso é um comentário javadoc
+         */
+
+public class Aula01OlaDevDojo {
+            /**
+             * documenta o método main
+             */
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+        // isso é um comentário de uma linha
+        /*
+            isso é um comentário
+            de múltiplas linhas
+         */
+    }
+}
