5aa04a6a90e4c908ccf29d11b905499760f902bb
João Pedro <jpedrosousans@gmail.com>
2026-09-10 19:57:27 -0300

    feat: adiciona Aula02TiposPrimitivos com os 8 tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -1,14 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
-        /**
-         * isso é um comentário javadoc
-         */
+/**
+ * isso é um comentário javadoc
+ */
 
 public class Aula01OlaDevDojo {
-            /**
-             * documenta o método main
-             */
-    public static void main(String[] args){
+    /**
+     * documenta o método main
+     */
+    public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
         // isso é um comentário de uma linha
         /*
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main() {
+        // int, long, double, float, byte, short, boolean, char
+        int age = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000.0;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+        System.out.println("A idade é " + age);
+        System.out.println(verdadeiro);
+        System.out.println(falso);
+        System.out.println("char "+ caractere);
+    }
+}
