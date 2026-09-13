26108d0dfc8142ba3d05847dbd348214c13fd02d
João Pedro <jpedrosousans@gmail.com>
2026-09-13 11:30:39 -0300

    feat: adiciona Aula04Operadores contendo operadores aritimeticos, relacionais, logicos e atribuicao

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,63 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero1 = 10;
+        int numero2 = 20;
+        double resultado = numero1 / (double) numero2;
+        System.out.println(resultado);
+
+        // %
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+        System.out.println("isDezIgualDez " + isDezIgualDez);
+        System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
+
+        // && (AND) || (OR)
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente >= 5000 || valorTotalContaPoupanca >= valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel " + isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800; //1800
+        bonus += 1000;       //2800
+        bonus -= 1000;       //1800
+        bonus *= 2;          //3600
+        bonus /= 2;          //1800
+        bonus %= 3;          //0
+        System.out.println("bonus " + bonus);
+
+        // ++ --
+        int contador1 = 0;
+        int contador2 = 0;
+        contador1 += 1;        //contador = contador +1
+        contador1++;          //contador = contador +1
+        contador1--;          //contador = contador -1
+        ++contador1;
+        --contador1;
+        System.out.println("contador1 " + contador1);
+        System.out.println("contador2 " + contador2++);
+        System.out.println("contador2 " + contador2);
+        System.out.println("contador2 " + ++contador2);
+    }
+}
